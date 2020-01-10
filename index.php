5a6fc603064370de44d68f73c2e6c62ad608ab3d
Votre Nom <odilecoutiez@gmail.com>
2020-01-10 16:34:38 +0100

    Fichier parent pour le host

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+        <title>Partie-VII</title>
+    </head>
+    <body>
+        <div class="">
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <button type="button"><a href="exo-1/index.php">Exercice-1</a></button>
+                </div>
+                <div class="col-md-6 text-center">
+                    <button type="button"><a href="exo-2/index.php">Exercice-2</a></button>
+                </div>
+            </div>
+            <div class="offset-4 col-md-4 text-center middleDiv">
+                <button type="button"><a href="exo-3/index.php">Exercice-3</a></button>
+            </div>
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <button type="button"><a href="exo-4/index.php">Exercice-4</a></button>
+                </div>
+                <div class="col-md-6 text-center">
+                    <button type="button"><a href="exo-5/index.php">Exercice-5</a></button>
+                </div> 
+            </div>
+            <div class="offset-4 col-md-4 text-center middleDiv">
+                <button type="button"><a href="exo-6/index.php">Exercice-6</a></button>
+            </div>
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <button type="button"><a href="exo-7/index.php">Exercice-7</a></button>
+                </div>
+                <div class="col-md-6 text-center">
+                    <button type="button"><a href="exo-8/index.php">Exercice-8</a></button>
+                </div> 
+            </div>
+        </div>
+    </body>
+</html>
